77398c7bc261b7eddb75fb7bca6783167ebe9a24
Jinnie-J <jjesweet@naver.com>
2021-02-03 12:43:17 +0900

    프로젝트 생성

---
+++ src/main/java/com/jinnie/guestbook/controller/GuestbookController.java
@@ -0,0 +1,33 @@
+package com.jinnie.guestbook.controller;
+
+import com.jinnie.guestbook.dto.PageRequestDTO;
+import com.jinnie.guestbook.service.GuestbookService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/guestbook")
+@Log4j2
+@RequiredArgsConstructor
+public class GuestbookController {
+
+    private final GuestbookService service;
+
+    @GetMapping({"/","/list"})
+    public String list(){
+        log.info("list..............");
+
+        return "/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        log.info("list........."+pageRequestDTO);
+        model.addAttribute("result",service.getList(pageRequestDTO));
+    }
+}
---
+++ src/main/java/com/jinnie/guestbook/service/GuestbookService.java
@@ -0,0 +1,35 @@
+package com.jinnie.guestbook.service;
+
+import com.jinnie.guestbook.dto.GuestbookDTO;
+import com.jinnie.guestbook.dto.PageRequestDTO;
+import com.jinnie.guestbook.entity.Guestbook;
+import com.jinnie.guestbook.dto.PageResultDTO;
+
+public interface GuestbookService {
+
+    Long register(GuestbookDTO dto);
+
+    PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO);
+
+    default Guestbook dtoToEntity(GuestbookDTO dto){
+        Guestbook entity = Guestbook.builder()
+                .gno(dto.getGno())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(dto.getWriter())
+                .build();
+        return entity;
+    }
+
+    default GuestbookDTO entityToDto(Guestbook entity){
+        GuestbookDTO dto = GuestbookDTO.builder()
+                .gno(entity.getGno())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+        return dto;
+    }
+}
